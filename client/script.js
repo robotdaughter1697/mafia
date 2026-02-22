26e052cf101ac909da49b2a8a0763605f04b4842
gabs <gabrielahouser1@gmail.com>
2026-02-21 22:04:36 -0500

    i got the websocket working i think but idrk

---
+++ client/script.js
@@ -0,0 +1,10 @@
+// this is for accepting the tic tac toe inputs
+
+import {io } from 'socket.io-client'
+
+
+
+
+const socket = io('http://localhost:3000')
+
+// const { Server } = require("socket.io");
--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
-// gabs
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path'); // Standard Node.js tool for paths
 
-const io = require('socket.io')(3000)
+// 1. Tell Express to serve files from the 'client' folder
+app.use(express.static(path.join(__dirname, '../client')));
 
-io.on('connection', socket => {
-    console.log(socket.id)
-})
+io.on('connection', (socket) => {
+    console.log('A player connected:', socket.id);
+});
+
+// 2. Start the server on port 3000
+const PORT = 3000;
+http.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
